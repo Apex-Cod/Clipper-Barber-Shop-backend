c86032ffaff4c55048e64b91ba1e543f5dcf715a
tichekiwar <kiwartiche@gmail.com>
2025-09-25 08:01:35 -0500

    Initial commit

---
+++ src/main/java/apex/code/clipperBarberShop/ClipperBarberShopApplication.java
@@ -0,0 +1,13 @@
+package apex.code.clipperBarberShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipperBarberShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipperBarberShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apex/code/clipperBarberShop/ClipperBarberShopApplicationTests.java
@@ -0,0 +1,13 @@
+package apex.code.clipperBarberShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipperBarberShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
